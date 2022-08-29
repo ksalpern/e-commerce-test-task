bc3cdd24af000454561e96246aa5487362454d45
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-29 12:12:56 +0300

    i redux-persist & add persistGate layer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
 
@@ -18,7 +19,9 @@ root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ApolloProvider>
   </React.StrictMode>
